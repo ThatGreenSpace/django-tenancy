ad626e4dd442efa13598d04cb6723f589f59c780
Alberto Resco Perez <alberto.resco.perez@thatgreenspace.com.sg>
2014-10-30 15:09:06 +0800

    First attemp of migrations for Django1.7

--- tenancy/models.py
+++ tenancy/models.py
@@ -237,7 +237,7 @@ class TenantModelBase(ModelBase):
             if getattr(Meta, 'proxy', False):
                 model = super_new(
                     cls, name, bases,
-                    dict(attrs, meta=meta(Meta, managed=False))
+                    dict(attrs, meta=meta(Meta, managed=True))
                 )
                 cls.references[model] = cls.reference(model, Meta)
             else:
@@ -257,7 +257,7 @@ class TenantModelBase(ModelBase):
                             related_names[m2m.name] = m2m.rel.related_name
                 model = super_new(
                     cls, name, bases,
-                    dict(attrs, Meta=meta(Meta, managed=False))
+                    dict(attrs, Meta=meta(Meta, managed=True))
                 )
                 cls.references[model] = cls.reference(model, Meta, related_names)
                 opts = model._meta
